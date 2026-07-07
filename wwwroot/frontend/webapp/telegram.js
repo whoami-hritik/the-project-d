dbf83ccbd7b5eac1e31683907745dfabf8487520
whoami_hritik <rajhritikk21@gmail.com>
2026-07-07 22:54:18 +0530

    update

--- wwwroot/frontend/webapp/telegram.js
+++ wwwroot/frontend/webapp/telegram.js
@@ -6,6 +6,13 @@ if(tg){
     tg.ready();
     tg.setHeaderColor('bg_color'); 
     // tg.enableClosingConfirmation(); 
+    if (tg.requestFullscreen) {
+        try {
+            tg.requestFullscreen();
+        } catch (e) {
+            console.warn("Telegram WebApp requestFullscreen not supported or blocked:", e);
+        }
+    }
 }
 
 export { tg };
